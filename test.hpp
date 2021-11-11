b536df9b13c772a45fa76c3f90c87dc796c17a7b
Ingmar Kole <ikole@student.codam.nl>
2021-11-11 15:35:34 +0100

    Feat: unit test + benchmark

--- test.hpp
+++ test.hpp
@@ -27,11 +27,17 @@
 // COLOR DEFINES //
 ///////////////////
 
-# define COLOR_RED		"\033[31m"
-# define COLOR_GREEN	"\033[32m"
-# define COLOR_RESET	"\033[0m"
-# define COLOR_YELLOW	"\033[33m"
-# define COLOR_BLUE		"\033[27m"
+# define COLOR_RED 		"\e[38;5;31m"
+# define COLOR_GREEN	"\e[32m"
+# define COLOR_BLUE 	"\e[38;5;51m"
+# define COLOR_BLU		"\e[38;5;190m"
+# define COLOR_ORANGE	"\e[38;5;214m"
+# define COLOR_PURPLE	"\e[38;5;98m"
+# define COLOR_YELLOW	"\e[38;5;220m"
+# define COLOR_PINK		"\e[38;5;205m"
+# define COLOR_YOS		"\e[38;5;226"
+
+# define COLOR_RESET	"\e[0m"
 
 //////////////
 // Messages //
@@ -41,17 +47,11 @@
 # define TEST_FAILURE "[" COLOR_RED	"KO" COLOR_RESET "] "
 
 
-////////////
-// MACROS //
-////////////
-
-# define STRESS_TEST()\
-	for (size_t i = 0; i < 50000; ++i)
 
 void	print_header(std::string header)
 {
-	std::cout << "-----------------";
-	std::cout << std::setw(15) << header;
+	std::cout << "-----------------" << COLOR_ORANGE;
+	std::cout << std::setw(15) << header << COLOR_RESET;
 	std::cout << "-----------------\n" << std::endl;
 };
 
@@ -61,6 +61,24 @@ void	insert_amount(ft_container& ft_con, std_container& std_con, T val, size_t n
 	for (size_t i = 0; i < n; ++i) {
 		ft_con.insert(val);
 		std_con.insert(val);
+	}
+}
+
+template< class ft_container, class std_container >
+void	push_back_increment(ft_container& ft_con, std_container& std_con, size_t n = 420)
+{
+	for (size_t i = 0; i < n; ++i) {
+		ft_con.push_back(i);
+		std_con.push_back(i);
+	}
+}
+
+template< class ft_container, class std_container >
+void	insert_increment(ft_container& ft_con, std_container& std_con, size_t n = 420)
+{
+	for (size_t i = 0; i < n; ++i) {
+		ft_con.insert(i);
+		std_con.insert(i);
 	}
 }
 
@@ -78,10 +96,11 @@ class Timer
 	private:
 		struct timeval	_time;
 		size_t			_duration;
+		size_t			_total_time;
 		size_t			_started_time;
 
 	public:
-		Timer() : _duration(0), _started_time(0) {}
+		Timer() : _duration(0), _total_time(0), _started_time(0) {}
 		
 		void reset_timer()
 		{
@@ -94,13 +113,50 @@ class Timer
 			_started_time = (_time.tv_sec * 1000 * 1000) + _time.tv_usec;
 		}
 
-		void end_timer()
+		size_t end_timer()
 		{
 			gettimeofday(&_time, NULL);
 			_duration += ((_time.tv_sec * 1000 * 1000) + _time.tv_usec) - _started_time;
+			_total_time += _duration;
+			return (_duration);
+		}
+
+		size_t	get_total_time()
+		{
+			return (_total_time);
+		}
+
+		void	end_and_print(const char* msg)
+		{
+			size_t duration = this->end_timer();
+			this->print_time(duration, msg);
+			this->reset_timer();
+		}
+
+		void	print_time(size_t& time, const char * msg)
+		{
+			std::cout	<< COLOR_PINK	<< std::setw(12) << std::right << msg\
+						<< COLOR_RESET  << " took: " \
+						<< COLOR_BLUE	<< std::setw(10) << std::right << time \
+						<< COLOR_RED	<< "µs" \
+						<< COLOR_RESET	<< std::endl;
 		}
 
 }; /* end of timer class */
+
+struct test
+{
+	private: 
+		int			_val;
+		std::string	_name;
+	public:
+		test(int val = 0, std::string name = "unknown") : _val(val), _name(name) {}
+		~test() {}
+
+	void	print_test() const {
+		std::cout << "val = " << _val << " name = " << _name << std::endl;
+	}
+}; /* end of test class */
 
 void	start_test(std::string name, void(*func)())
 {
@@ -118,6 +174,43 @@ void	compare(ft_val& ft_con, std_val& std_con, bool(*comp)(ft_val&, std_val&), c
 		std::cout << TEST_FAILURE;
 		std::cout << "test: " << test << std::endl;
 	}
+}
+
+template < class T1, class T2 >
+void	print_difference(T1 expected, T2 received)
+{
+	std::cout	<< "expected value: " << expected << std::endl\
+				<< "received value: " << received << std::endl;
+}
+
+template <class ft_vec, class std_vec>
+bool	compare_these_iterators_yo(ft_vec& ft, std_vec& std)
+{
+	if (*ft != *std) {
+		print_difference(*std, *ft);
+		return false;
+	}
+	return (true);
+}
+
+template <class ft_vec, class std_vec>
+bool	compare_these_values_yo(ft_vec& ft, std_vec& std)
+{
+	if (ft != std) {
+		print_difference(std, ft);
+		return false;
+	}
+	return (true);
+}
+
+bool compare_exceptions(std::string& ft_exc, std::string& std_exc)
+{
+	if (ft_exc != std_exc) {
+		print_difference(std_exc, ft_exc);
+		return false;
+	}
+	else
+		return true;
 }
 
 #endif
---
+++ vector_tests/vector_unit.cpp
@@ -0,0 +1,414 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*              ,---------.     .-''-.      .-'''-.  ,---------.              */
+/*              \          \  .'_ _   \    / _     \ \          \             */
+/*               `--.  ,---' / ( ` )   '  (`' )/`--'  `--.  ,---'             */
+/*                  |   \   . (_ o _)  | (_ o _).        |   \                */
+/*                  :_ _:   |  (_,_)___|  (_,_). '.      :_ _:                */
+/*                  (_I_)   '  \   .---. .---.  \  :     (_I_)                */
+/*                 (_(=)_)   \  `-'    / \    `-'  |    (_(=)_)               */
+/*                  (_I_)     \       /   \       /      (_I_)                */
+/*                  '---'      `'-..-'     `-...-'       '---'                */
+/*                                                                            */
+/*                   Kingmar  |  https://github.com/K1ngmar                   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <test.hpp>
+#include <vector.hpp>
+#include <vector>
+
+template <class ft_vec, class std_vec>
+bool	compare_these_vectors_yo(ft_vec& ft, std_vec& std)
+{
+	if (ft.size() != std.size()) {
+		std::cout << "\nwrong size() :(" << std::endl;
+		print_difference(std.size(), ft.size());
+		return false;
+	}
+	if (ft.max_size() != std.max_size()) {
+		std::cout << "\nwrong max_size() :(" << std::endl;
+		print_difference(std.max_size(), ft.max_size());
+		return false;
+	}
+	if (ft.empty() != std.empty()) {
+		std::cout << "\nwrong empty() :(" << std::endl;
+		print_difference(std.empty(), ft.empty());
+		return false;
+	}
+	for(size_t i = 0; i < ft.size(); ++i) {
+		if (ft[i] != std[i]) {
+			std::cout << "\nindex: " << i << std::endl;
+			print_difference(std[i], ft[i]);
+			return false;
+		}
+	}
+	return (true);
+}
+
+/////////////////
+// CONSTRUCTOR //
+/////////////////
+static void constructor_test()
+{
+
+/* default constructor */
+
+	ft::vector<int> ft_dc;
+	std::vector<int> std_dc;
+	compare(ft_dc, std_dc, compare_these_vectors_yo, "Default constructor");
+
+// /* set allocator */
+	ft::vector<std::string, std::allocator<std::string> > ft_sa;
+	std::vector<std::string, std::allocator<std::string> > std_sa;
+	compare(ft_sa, std_sa, compare_these_vectors_yo, "Set allocator");
+
+// /* fill constructor */
+	ft::vector<int> ft_fc(42, 'a');
+	std::vector<int> std_fc(42, 'a');
+	compare(ft_dc, std_dc, compare_these_vectors_yo, "Fill constructor");
+
+// /* range constructor */
+	ft::vector<int> ft_rc(ft_fc.begin(), ft_fc.end() - 21);
+	std::vector<int> std_rc(std_fc.begin(), std_fc.end() - 21);
+	compare(ft_rc, std_rc, compare_these_vectors_yo, "range constructor");
+
+// /* copy constructor */
+	ft::vector<int> ft_cc(ft_rc);
+	std::vector<int> std_cc(std_rc);
+	compare(ft_cc, std_cc, compare_these_vectors_yo, "Copy constructor");
+
+// /* assigantion operator */
+	ft::vector<int> ft_ass = ft_cc;
+	std::vector<int> std_ass = std_cc;
+	compare(ft_ass, std_ass, compare_these_vectors_yo, "Assignation operator");
+}
+
+///////////////////
+// iterator test //
+///////////////////
+static void iterator_test()
+{
+	ft::vector<int>								ft_vec;
+	std::vector<int>							std_vec;
+	ft::vector<int>::iterator					ft_itr;
+	std::vector<int>::iterator					std_itr;
+	ft::vector<int>::const_iterator				ft_citr;
+	std::vector<int>::const_iterator			std_citr;
+
+	for (size_t i = 0; i < 21; ++i)
+		push_back_amount(ft_vec, std_vec, rand(), 1);
+
+/* testing begin */
+
+	ft_itr = ft_vec.begin();
+	std_itr = std_vec.begin();
+	compare(ft_itr, std_itr, compare_these_iterators_yo, "begin()");
+
+/* testing const begin */
+	ft_citr = ft_vec.begin();
+	std_citr = std_vec.begin();
+	compare(ft_citr, std_citr, compare_these_iterators_yo, "const begin()");
+
+
+	/* do some reverse iterator test you imbecile */
+	
+
+/* testing end() */
+	ft_itr = ft_vec.end() - 1;
+	std_itr = std_vec.end() - 1;
+	compare(ft_itr, std_itr, compare_these_iterators_yo, "end() - 1");
+
+/* testing const end() */
+	ft_citr = ft_vec.end() - 1;
+	std_citr = std_vec.end() - 1;
+	compare(ft_citr, std_citr, compare_these_iterators_yo, "const end() - 1");
+
+
+	/* do some reverse const iterator test you imbecile */
+}
+
+//////////////
+// CAPACITY //
+//////////////
+static void capacity_test()
+{
+	ft::vector<int>	ft_vec;
+	std::vector<int> std_vec;
+
+	ft_vec.reserve(42);
+	std_vec.reserve(42);
+	compare(ft_vec, std_vec, compare_these_vectors_yo, "reserve(42)");
+
+	ft_vec.resize(69, 420);
+	std_vec.resize(69, 420);
+	compare(ft_vec, std_vec, compare_these_vectors_yo, "resize(69, 420)");
+
+	ft_vec.resize(21, 69);
+	std_vec.resize(21, 69);
+	compare(ft_vec, std_vec, compare_these_vectors_yo, "rezize(21, 69)");
+
+	ft_vec.resize(5000, 420);
+	std_vec.resize(5000, 420);
+	compare(ft_vec, std_vec, compare_these_vectors_yo, "rezize(5000, 420)");
+
+	/* je boi gaat hier ergens dood ofzo */
+
+	ft_vec.resize(21, 1337);
+	std_vec.resize(21, 1337);
+	compare(ft_vec, std_vec, compare_these_vectors_yo, "rezize(5000, 420)");
+
+	std::string ft_msg;
+	std::string std_msg;
+	try {
+		ft_vec.reserve(-1);
+	} catch (std::exception &e) {
+		ft_msg = std::string(e.what());
+	}
+	try {
+		std_vec.reserve(-1);
+	} catch (std::exception &e) {
+		std_msg = std::string(e.what());
+	}
+	compare(ft_msg, std_msg, compare_exceptions, "reserve(-1)");
+}
+
+////////////////////
+// ELEMENT ACCESS //
+////////////////////
+static void element_access_test()
+{
+	ft::vector<size_t> ft_vec;
+	std::vector<size_t> std_vec;
+
+	push_back_increment(ft_vec, std_vec, 1337);
+	compare(ft_vec, std_vec, compare_these_vectors_yo, "[] operator");
+
+/* subscript operator */
+	int j = ft_vec.size() - 1;
+	for (int i = 0; i != j; i++) {
+		ft_vec[j] = std_vec[i];
+		std_vec[j] = std_vec[i];
+	}
+	compare(ft_vec, std_vec, compare_these_vectors_yo, "reversed with [] operator");
+
+/* at */
+	std::string ft_msg;
+	std::string std_msg;
+	try {
+		for (size_t i = 0; i < ft_vec.size() + 1; i++)
+			if (ft_vec.at(i) != std_vec.at(i))
+				break ;
+	} catch (std::exception &e) {
+		ft_msg = std::string(e.what());
+	}
+
+	try {
+		for (size_t i = 0; i < std_vec.size() + 1; i++)
+			std_vec.at(i);
+	} catch (std::exception &e) {
+		std_msg = std::string(e.what());
+	}
+
+	compare(ft_msg, std_msg, compare_exceptions, "at(ft_vec.size() + 1)");
+
+	try {
+		std::cout << ft_vec.at(-1) << std::endl;
+	} catch (std::exception &e) {
+		ft_msg = std::string(e.what());
+	}
+
+	try {
+		std::cout << std_vec.at(-1) << std::endl;
+	} catch (std::exception &e) {
+		std_msg = std::string(e.what());
+	}
+
+	compare(ft_msg, std_msg, compare_exceptions, "at(-1");
+
+/* front && back */
+	compare(ft_vec.front(), std_vec.front(), compare_these_values_yo, "front()");
+	compare(ft_vec.back(), std_vec.back(), compare_these_values_yo, "back()");
+}
+
+///////////////
+// MODIFIERS //
+///////////////
+static void modifier_test()
+{
+	ft::vector<std::string>				ft_vec(2000, "MANY STRINGS YO");
+	std::vector<std::string>			std_vec(2000, "MANY STRINGS YO");
+	ft::vector<std::string>				ft_empty;
+	std::vector<std::string>			std_empty;
+
+/* iterator range assign */
+	ft_empty.assign(ft_vec.begin() + 42, ft_vec.begin() + 1337);
+	std_empty.assign(std_vec.begin() + 42, std_vec.begin() + 1337);
+	compare(ft_empty, std_empty, compare_these_vectors_yo, "assign(std_vec.begin() + 42, std_vec.begin + 1337)");
+
+/* fill assign */
+	std::string assigned("we just got assigned yo");
+	ft_empty.assign(420, assigned);
+	std_empty.assign(420, assigned);
+	compare(ft_empty, std_empty, compare_these_vectors_yo, "assign(420, assigned)");
+
+	ft_empty.assign(5000, std::string("wejoooowww"));
+	std_empty.assign(5000, std::string("wejoooowww"));
+	compare(ft_empty, std_empty, compare_these_vectors_yo, "assign(5000, \"wejoow\")");
+	
+/* push back */
+	for (size_t i = 0; i < 6969; ++i) {
+		ft_empty.push_back(std::to_string(i));
+		std_empty.push_back(std::to_string(i));
+	}
+	compare(ft_empty, std_empty, compare_these_vectors_yo, "push back increment");
+
+/* pop back */
+	for (size_t i = 0; i < 6969; i++) {
+		ft_empty.pop_back();
+		std_empty.pop_back();
+	}
+	compare(ft_empty, std_empty, compare_these_vectors_yo, "pop_back() * 6969");
+
+/* insert */
+	ft_empty.insert(ft_empty.begin() + 42, "LEGENDARY");
+	std_empty.insert(std_empty.begin() + 42, "LEGENDARY");
+	compare(ft_empty, std_empty, compare_these_vectors_yo, "insert(begin() + 42, \"LEGENDARY\")");
+
+/* range erase */
+	ft_empty.erase(ft_empty.begin(), ft_empty.end() - 1);
+	std_empty.erase(std_empty.begin(), std_empty.end() - 1);
+	compare(ft_empty, std_empty, compare_these_vectors_yo, "begin(), end() - 1");
+
+/* fill insert */
+	ft_empty.insert(ft_empty.end() - 1, 1337, "living on the edge");
+	std_empty.insert(std_empty.end() - 1, 1337, "living on the edge");
+	compare(ft_empty, std_empty, compare_these_vectors_yo, "end() - 1, 1337, \"living on the edge");
+
+/* element erase */
+	for (size_t i = 0; i < ft_empty.size(); i++) {
+		ft_empty.erase(ft_empty.begin() + i);
+		std_empty.erase(std_empty.begin() + i);
+
+	}
+	compare(ft_empty, std_empty, compare_these_vectors_yo, "erase(begin() + i)");
+
+/* Range insert */
+	ft_empty.insert(ft_empty.begin(), ft_vec.begin(), ft_vec.end());
+	std_empty.insert(std_empty.begin(), std_vec.begin(), std_vec.end());
+	compare(ft_empty, std_empty, compare_these_vectors_yo, "empty.begin(), vec.begin(), vec.end()");
+
+/* clear test */
+	ft_empty.clear();
+	std_empty.clear();
+	compare(ft_empty, std_empty, compare_these_vectors_yo, "clear()");
+
+/* swaperoo */
+	ft_empty.insert(ft_empty.begin(), 42, "About to get swapped");
+	std_empty.insert(std_empty.begin(), 42, "About to get swapped");
+	ft_empty.swap(ft_vec);
+	std_empty.swap(std_vec);
+	compare(ft_empty, std_empty, compare_these_vectors_yo, "swap()");
+	compare(ft_vec, std_vec, compare_these_vectors_yo, "swap()");
+}
+
+template< class Container >
+static size_t benchmark_test(Container& con, Container& vec)
+{
+	Timer	time;
+	size_t	duration;
+
+	time.start_timer();
+	for (size_t i = 0; i < 500000; ++i)
+		con.push_back(test(i));
+	time.end_and_print("push_back");
+
+	time.start_timer();
+	con.insert(con.begin(), 250000, test(1, "insert"));
+	time.end_and_print("fill insert");
+
+	time.start_timer();
+	while (con.size() > 1337)
+		con.pop_back();
+	time.end_and_print("pop back");
+	
+	time.start_timer();
+	for (size_t i = 0; i < 42069; ++i)
+		vec.swap(con);
+	time.end_and_print("swap");
+
+	vec.insert(vec.begin(), 42069, test(1, "erase"));
+
+	time.start_timer();
+	vec.erase(vec.begin(), vec.end() - 2500);
+	time.end_and_print("erase range");
+
+	vec.insert(vec.begin(), 42069, test(1, "erase"));
+
+	time.start_timer();
+	vec.clear();
+	time.end_and_print("clear");
+
+	time.start_timer();
+	for (size_t i = 0; i < 50000; ++i) {
+		vec.reserve(250);
+		vec.reserve(500000);
+	}
+	time.end_and_print("Reserve");
+
+	for (size_t i = 0; i < vec.capacity(); i++)
+		vec.push_back(test(i));
+
+	time.start_timer();
+	for (size_t i = 0; i < 500; ++i) {
+		vec.resize(25000);
+		vec.resize(13370);
+	}
+	time.end_and_print("Resize");
+	
+	time.start_timer();
+	for (size_t i = 0; i < 50; ++i) {
+		vec.assign(420, test(69));
+		vec.assign(200000, test(69));
+	}
+	time.end_and_print("assign");
+	return (time.get_total_time());
+}
+
+static void benchmark()
+{
+	double ft_duration, std_duration;
+	std::cout << "\n   -----BENCHMARK ft::vector-----\n";
+	ft::vector<test> ft_con;
+	ft::vector<test> ft_vec;
+	ft_duration = benchmark_test(ft_con, ft_vec);
+
+	std::cout << "\n  -----BENCHMARK std::vector-----\n";
+	std::vector<test> std_con;
+	std::vector<test> std_vec;
+	std_duration = benchmark_test(std_con, std_vec);
+
+	std::cout << "ft::vector was " << ((std_duration / ft_duration) * 100) << "% faster\n";
+
+	// std::vector<test> std_vec;
+}
+
+void	vector_unit()
+{
+	print_header("Vector member functions");
+
+	start_test("constructor", constructor_test);
+	start_test("iterator", iterator_test);
+	start_test("capacity", capacity_test); 
+	start_test("element access", element_access_test); 
+	start_test("modifier test", modifier_test);
+	benchmark();
+}
+
+
+int main()
+{
+	srand(42);
+	vector_unit();
+	return(0);
+}
