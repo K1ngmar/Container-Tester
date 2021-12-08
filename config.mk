c46924fa0d0b3038441a05c24f8347896f92b615
Ingmar Kole <ikole@student.codam.nl>
2021-12-08 17:03:55 +0100

    Feat: added config file so its more user friendly

---
+++ config.mk
@@ -0,0 +1,44 @@
+#  **************************************************************************  #
+#                                                                              #
+#         ,--,  .---.  .-. .-. _______  .--.  ,-..-. .-.,---.  ,---.           #
+#       .' .') / .-. ) |  \| ||__   __|/ /\ \ |(||  \| || .-'  | .-.\          #
+#       |  |(_)| | |(_)|   | |  )| |  / /__\ \(_)|   | || `-.  | `-'/          #
+#       \  \   | | | | | |\  | (_) |  |  __  || || |\  || .-'  |   (           #
+#        \  `-.\ `-' / | | |)|   | |  | |  |)|| || | |)||  `--.| |\ \          #
+#         \____\)---'  /(  (_)   `-'  |_|  (_)`-'/(  (_)/( __.'|_| \)\         #
+#              (_)    (__)                      (__)   (__)        (__)        #
+#               _______ ,---.     .---.  _______ ,---.  ,---.                  #
+#              |__   __|| .-'    ( .-._)|__   __|| .-'  | .-.\                 #
+#                )| |   | `-.   (_) \     )| |   | `-.  | `-'/                 #
+#               (_) |   | .-'   _  \ \   (_) |   | .-'  |   (                  #
+#                 | |   |  `--.( `-'  )    | |   |  `--.| |\ \                 #
+#                 `-'   /( __.' `----'     `-'   /( __.'|_| \)\                #
+#                      (__)                     (__)        (__)               #
+#                                                                              #
+#                    Kingmar  |  https://github.com/K1ngmar                    #
+#                                                                              #
+#  **************************************************************************  #
+
+_CONTAINERS_LOC	=	../containers
+
+DEQUE_LOC		=	$(CONTAINERS_LOC)/deque/
+
+UTILITY_LOC		=	$(CONTAINERS_LOC)/utility/
+
+ITERATOR_LOC	=	$(CONTAINERS_LOC)/iterator/
+
+MAP_LOC			=	$(CONTAINERS_LOC)/map/
+
+PAIR_LOC		=	$(CONTAINERS_LOC)/pair/
+
+SET_LOC			=	$(CONTAINERS_LOC)/set/
+
+STACK_LOC		=	$(CONTAINERS_LOC)/stack/
+
+VECTOR_LOC		=	$(CONTAINERS_LOC)/vector/
+
+OTHER_LOC		=	$(CONTAINERS_LOC)/sfinae/					\
+					$(CONTAINERS_LOC)/unorederd_map/			\
+					$(CONTAINERS_LOC)/lexicographical_compare/
+
+CONTAINERS_LOC	= $(addprefix -I, $(_CONTAINERS_LOC))
